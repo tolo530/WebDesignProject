29b24d53c4cffbd091a4522c0f245d4baf37f697
Pablo PT <ptx@protonmail.com>
2019-12-07 00:22:22 +0000

    work

---
+++ js/loadcontent.js
@@ -0,0 +1,11 @@
+function active_drink(num) {
+    
+    if(num == 1){
+        document.getElementById("drinksection").innerHTML = "nice"
+    }
+
+    if(num == 2){
+        document.getElementById("drinksection").innerHTML = "jeje"
+    }
+
+}
